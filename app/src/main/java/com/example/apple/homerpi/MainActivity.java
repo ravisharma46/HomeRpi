a5826863394d4930d64e3bc61cc2e9f6b636e03e
ravisharma46 <ravi.ravi.sharma46@gmail.com>
2019-04-07 13:17:53 +0530

    final submit

--- app/src/main/java/com/example/apple/homerpi/MainActivity.java
+++ app/src/main/java/com/example/apple/homerpi/MainActivity.java
@@ -48,10 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
                 Intent intent=new Intent(MainActivity.this,verifyPhoneActivity.class);
                 intent.putExtra("mobile",mobile);
                 startActivity(intent);
